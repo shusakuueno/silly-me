175f0ac8192b8688b308cb45bbdc5d5b9d61a494
shusaku.ueno <shu.ueno@gmail.com>
2019-11-23 16:15:00 +0000

    user

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+User.create!(name: "ueno",
+             email: "ueno@gmail.com",
+             password:                 "ueno",
+             password_confirmation:    "ueno",
+             admin: true )
